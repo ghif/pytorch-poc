a1b737541400b8b864f86eee7d04c88cde49c6ba
Muhammad Ghifary <mghifary@192.168.1.5>
2024-01-04 10:23:02 +0700

    resnet results

--- 2-cifar_classification.py
+++ 2-cifar_classification.py
@@ -34,7 +34,6 @@ train_transform = transforms.Compose(
     [
         v2.RandAugment(),
         transforms.ToTensor(),
-        # v2.RandAugment(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ]
 )
